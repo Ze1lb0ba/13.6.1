c0ffcfa3e17ff3ecca4a5fb048f00eaf09d6aed3
stank <stankevich_dg@mail.ru>
2023-01-17 20:15:50 +0400

    Добавьте файлы проекта.

---
+++ 13.6.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var path = @"C:\Courses C#\Module13\Text1.txt";
+
+        var timer1 = new Stopwatch();
+
+        timer1.Start();          
+        var textFile = new List<string>();
+        var myList = File.ReadAllLines(path);      
+        foreach(var item in myList)
+        {
+            textFile.Add(item);
+        }
+        timer1.Stop();
+
+        var timer2 = new Stopwatch();
+
+        timer2.Start();
+        var textFile2 = new LinkedList<string>();
+        var myList2 = File.ReadAllLines(path);
+        foreach(var item in myList2)
+        {
+            textFile2.AddLast(item);
+        }
+        timer2.Stop();
+
+        Console.WriteLine("Время работы List: {0} милесекунд.", timer1.ElapsedMilliseconds);
+        Console.WriteLine("Время работы LinkedList: {0} милесекунд.", timer2.ElapsedMilliseconds);
+
+    }
+}
